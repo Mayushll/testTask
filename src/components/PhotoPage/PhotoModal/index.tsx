8046eb1326f5fe7e2446615ac060a7e477b03c21
MayushiiChan <mayushiidesu@mail.ru>
2022-02-12 16:22:52 +0500

    Fixed modal styles, root html

--- src/components/PhotoPage/PhotoModal/index.tsx
+++ src/components/PhotoPage/PhotoModal/index.tsx
@@ -1,22 +1,25 @@
 import React, {Dispatch, SetStateAction} from 'react';
-import {Box, Modal} from "@mui/material";
+import {Box, Modal, styled} from "@mui/material";
 interface Props{
     open: boolean
     setOpen: Dispatch<SetStateAction<boolean>>;
     modalImage: string;
 }
-const style = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    width: '632px',
-    height: '632px',
-    transform: 'translate(-50%, -50%)',
-    bgcolor: 'background.paper',
-    border: '2px solid #000',
-    boxShadow: 24,
-    p: 4,
-};
+
+const Wrapper = styled(Box)`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 632px;
+  height: 632px;
+  padding: 32px;
+  background-color: #F1E9D9;
+  @media(max-width: 632px){
+    width: 100%;
+    height: auto;
+  }
+`
 export const PhotoModal = ({open, setOpen, modalImage}: Props) => {
     const handleClose = () => {
         setOpen(false);
@@ -29,9 +32,9 @@ export const PhotoModal = ({open, setOpen, modalImage}: Props) => {
                 aria-labelledby="modal-modal-title"
                 aria-describedby="modal-modal-description"
             >
-                <Box sx={style}>
+                <Wrapper>
                     <img src={modalImage} alt={modalImage}/>
-                </Box>
+                </Wrapper>
             </Modal>
         </div>
     );
